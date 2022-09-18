0116dc56a17f482f6406374efd27edc61a132915
Helen <you@example.com>
2022-09-18 22:35:47 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+if (number1 % 2 == 0)
+{
+    Console.WriteLine("Число " + number1 + " чётное");
+}
+else
+{
+    Console.WriteLine("Число " + number1 + " нечётное");
+}
