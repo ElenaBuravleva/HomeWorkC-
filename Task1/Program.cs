e6ab6df462e68b6a1db7de7648f639012dfabbc4
Helen <you@example.com>
2022-09-18 21:04:50 +0300

    Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 == number2)
+{
+    Console.WriteLine("Введено одно и то же число");
+}
+else
+{    int Max;
+if (number1 > number2)
+{
+    Max = number1;
+}
+else
+{
+    Max = number2;
+}
+Console.WriteLine("max = " + Max);
+}
