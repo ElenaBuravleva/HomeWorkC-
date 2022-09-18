53edf65946794d281b8b3bd353c9cc8662bf6141
Helen <you@example.com>
2022-09-18 21:14:30 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int Max = number1;
+if (Max < number2)
+{
+    Max = number2;
+    if (Max < number3)
+    {
+        Max = number3;
+    }
+}
+else if (Max < number3)
+{
+    Max = number3;
+}
+Console.WriteLine("Максимум из трех чисел: " + Max);
