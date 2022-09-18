de6396b45f1ea6484f0975844812bcd51740d61d
Helen <you@example.com>
2022-09-18 22:54:00 +0300

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0)
+{
+    Console.WriteLine("Вы ввели отрицательное число");
+}
+else 
+{
+    int count = 0;
+    while (count <= number)
+    {
+        Console.WriteLine(count);
+        count = count + 2;
+    }
+}
