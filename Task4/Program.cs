4d602764709ce3fbf59bca0e04aabab12c920055
Helen <you@example.com>
2022-09-19 14:37:23 +0300

    Task4 дополнен условием <=1

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,11 +1,16 @@
 ﻿Console.WriteLine("Введите положительное число");
 int number = Convert.ToInt32(Console.ReadLine());
-if (number <= 0)
+if (number < 0)
 {
-    Console.WriteLine("Вы ввели отрицательное число либо 0");
+    Console.WriteLine("Вы ввели отрицательное число");
 }
-else 
+else if (number <= 1)
 {
+    Console.WriteLine("Четных чисел нет");
+}
+else
+{
+    Console.WriteLine("Четные числа:");
     int count = 2;
     while (count <= number)
     {
