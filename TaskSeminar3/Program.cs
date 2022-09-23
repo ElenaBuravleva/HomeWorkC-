fc4bb09f4a19fea8ac88abfddb5a0e88596797df
Helen <you@example.com>
2022-09-23 22:06:37 +0300

    задачи 3 семинара и 4 допзадачи с заголовками

--- TaskSeminar3/Program.cs
+++ TaskSeminar3/Program.cs
@@ -1,16 +1,17 @@
 ﻿void Zadacha19()
 {
+    Console.WriteLine("Задача о палиндроме");
     Console.WriteLine("Введите пятизначное число");
     int number = Convert.ToInt32(Console.ReadLine());
     if (Math.Abs(number) > 99999 || Math.Abs(number) < 10000)
     {
-        Console.WriteLine("Число не пятизначное");
+        Console.WriteLine("Число не является пятизначным");
     }
     else
     {
-        int two = (number / 1000) % 10;
-        int four = number / 10;
-        if (number / 10000 == number % 10 && two == four % 10)
+        int second = (number / 1000) % 10;
+        int fourth = number / 10;
+        if (number / 10000 == number % 10 && second == fourth % 10)
         {
             Console.WriteLine("Введенное число является палиндромом");
         }
@@ -21,10 +22,11 @@
 
     }
 }
-Zadacha19();
 
+// № 21 с ручным вводом координат точек
 void Zadacha21Input()
 {
+    Console.WriteLine("Нахождение расстояния между точками");
     Console.WriteLine("Введите координату х первой точки");
     int x1 = Convert.ToInt32(Console.ReadLine());
     Console.WriteLine("Введите координату y первой точки");
@@ -37,13 +39,14 @@ void Zadacha21Input()
     int y2 = Convert.ToInt32(Console.ReadLine());
     Console.WriteLine("Введите координату z второй точки");
     int z2 = Convert.ToInt32(Console.ReadLine());
-    double R = Math.Sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1) + (z2-z1)*(z2-z1));
-    Console.WriteLine("Расстояние между точками = "  + R);
+    double D = Math.Sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1) + (z2-z1)*(z2-z1));
+    Console.WriteLine("Расстояние между точками  D = "  + D);
 }
-Zadacha21Input();
 
+// № 21 с автоматическим вводом координат случайных координат точкек А и В
 void Zadacha21Random()
 {
+    Console.WriteLine("Нахождение расстояния между двумя случайными точками");
     Random random = new Random();
     int x1 = random.Next(-10,10);
     int y1 = random.Next(-10,10);
@@ -52,13 +55,14 @@ void Zadacha21Random()
     int y2 = random.Next(-10,10);
     int z2 = random.Next(-10,10);
     Console.WriteLine ($"A({x1},{y1},{z1}), B({x2},{y2},{z2})");
-    double R = Math.Sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1) + (z2-z1)*(z2-z1));
-    Console.WriteLine("Расстояние между точками A и В = "  + R);
+    double d = Math.Sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1) + (z2-z1)*(z2-z1));
+    Console.WriteLine("Расстояние между точками A и В  d = "  + d);
 }
-Zadacha21Random();
+
 
 void Zadacha23()
 {
+    Console.WriteLine("Вывод таблицы кубов чисел до заданного числа");
     Console.WriteLine("Введите число");
     int N = Convert.ToInt32(Console.ReadLine());
     int i = 1;
@@ -71,10 +75,11 @@ void Zadacha23()
         i++;
     }
 }
-Zadacha23();
+
 
 void DopZadacha1()
 {
+    Console.WriteLine("Значение Х при заданном Y");
     Console.WriteLine("Введите число");
     float x = Convert.ToInt32(Console.ReadLine());
     double y;
@@ -86,12 +91,13 @@ void DopZadacha1()
     {
         y = 1 - 2*Math.Sin(Math.Pow(x, 2));
     }
-    Console.WriteLine(y);
+    Console.WriteLine("При х = " + x + ", y = " + y);
 }
-DopZadacha1();
+
 
 void DopZadacha2()
 {
+    Console.WriteLine("Кратность 3 суммы цифр заданного числа");
     Console.WriteLine("Введите трёхзначное число");
     int number = Convert.ToInt32(Console.ReadLine());
     if (Math.Abs(number) > 999 || Math.Abs(number) < 100)
@@ -113,10 +119,11 @@ void DopZadacha2()
         }    
     }
 }
-DopZadacha2();
+
 
 void DopZadacha3()
 {
+    Console.WriteLine("Нахождение цифр 4 и 7 в трехзначном числе");
     Console.WriteLine("Введите трёхзначное число");
     int number = Convert.ToInt32(Console.ReadLine());
     if (Math.Abs(number) > 999 || Math.Abs(number) < 100)
@@ -136,25 +143,34 @@ void DopZadacha3()
         {
             if (array[i] == 4)
             {
-                Console.WriteLine("Число содержит цифру 4");
                 Priznak4 = 1;
             } 
             if (array[i] == 7) 
             {
-                Console.WriteLine("Число содержит цифру 7"); 
                 Priznak7 = 1;
             }
         }
         if (Priznak4 == 0 && Priznak7 == 0)
         {
             Console.WriteLine("Число не содержит цифр 4 и 7");
-        } 
+        }
+        else 
+        {
+            if (Priznak4 == 1)
+            {
+                Console.WriteLine("Число содержит цифру 4");
+            }
+            if (Priznak7 == 1)
+            {
+                Console.WriteLine("Число содержит цифру 7");
+            }
+        }
     }
 }
-DopZadacha3();
 
 void DopZadacha4()
 {
+    Console.WriteLine("Заполнение массива");
     int [] array = new int[10];
     int length = array.Length;
     int index = 0;
@@ -165,4 +181,12 @@ void DopZadacha4()
         index++;
     }
 }
+
+Zadacha19();
+Zadacha21Input();
+Zadacha21Random();
+Zadacha23();
+DopZadacha1();
+DopZadacha2();
+DopZadacha3();
 DopZadacha4();
